b2336a4e8bfa0234084013e9e9d1ab3dcae455d7
Melkizedeki <mmagota2@gmail.com>
2025-06-02 09:20:18 +0300

    The commit after debug card error and refactring the card

--- lib/pages/knowledge_post.dart
+++ lib/pages/knowledge_post.dart
@@ -43,45 +43,141 @@ class _KnowledgePostPageState extends State<KnowledgePostPage> {
                   itemCount: posts.length,
                   itemBuilder: (context, index) {
                     final post = posts[index];
-                    print("THis is image url${post['pictureUrl']}");
+                    print("THis is image url ${post['pictureUrl']}");
 
-                    return GFCard(
-                      boxFit: BoxFit.cover,
-                      image: Image.network(
-                        "https://media.istockphoto.com/id/1140829787/photo/sunset-at-savannah-plains.jpg?s=1024x1024&w=is&k=20&c=7Eraf7pY9AT-okf8710mu1etFEyOBOlrRK6vLfFo0s4=",
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
+                    return Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      title: GFListTile(
-                        title: Text(
-                          post['title'] ?? 'No Title',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        icon: Row(
-                          children: [
-                            const Icon(Icons.person),
-                            const SizedBox(
-                              width: 5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
                             ),
-                            const CircleAvatar(
-                              radius: 16,
-                              // backgroundImage:
-                              // AssetImage('assets/images/user.png'),
-                            )
-                          ],
-                        ),
-                        subTitle: Text(
-                          post['caption'] ?? 'No Caption',
-                          style: const TextStyle(fontSize: 14),
-                        ),
-                        description: Text(
-                          "Posted by ${post['user']['username']} on ${post['createdAt']}",
-                          style: const TextStyle(fontSize: 12),
-                        ),
+                            child: post['pictureUrl'] != null
+                                ? Image.network(
+                                    post['pictureUrl'],
+                                    width: double.infinity,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  )
+                                : const SizedBox.shrink(),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 4,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  post['title'] ?? 'No Title',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                Text(
+                                  post['caption'] ?? 'No no Caption',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Row(
+                                  children: [
+                                    CircleAvatar(),
+                                    SizedBox(
+                                      width: 8,
+                                    ),
+                                    Text(
+                                      post['user']['username'] ?? 'Unknown',
+                                      style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Spacer(),
+                                    ElevatedButton(
+                                        onPressed: null, child: Text("View"))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     );
-                    // ListTile(
+
+                    //  GFCard(
+                    //   boxFit: BoxFit.cover,
+                    //   image: Image.asset('assets/images/icon.png'),
+                    //   title: GFListTile(
+                    //     avatar: GFAvatar(
+                    //       backgroundImage: AssetImage('your asset image'),
+                    //     ),
+                    //     title: Text('Card Title'),
+                    //     subTitle: Text('Card Sub Title'),
+                    //   ),
+                    //   content:
+                    //       Text("Some quick example text to build on the card"),
+                    //   buttonBar: GFButtonBar(
+                    //     children: <Widget>[
+                    //       GFButton(
+                    //         onPressed: () {},
+                    //         text: 'Buy',
+                    //       ),
+                    //       GFButton(
+                    //         onPressed: () {},
+                    //         text: 'Cancel',
+                    //       ),
+                    //     ],
+                    //   ),
+                    // );
+
+                    //  GFCard(
+                    //   boxFit: BoxFit.cover,
+                    //   image: post['pictureUrl'] != null
+                    //   ? Image.network(
+                    //       post['pictureUrl'],
+                    //       width: 50,
+                    //       height: 50,
+                    //       fit: BoxFit.cover,
+                    //     )
+                    //   : null,
+                    //   title: GFListTile(
+                    //     title: Text(
+                    //       post['title'] ?? 'No Title',
+                    //       style: TextStyle(fontWeight: FontWeight.bold),
+                    //     ),
+                    //     icon: Row(
+                    //       children: [
+                    //         const Icon(Icons.person),
+                    //         const SizedBox(
+                    //           width: 5,
+                    //         ),
+                    //         const CircleAvatar(
+                    //           radius: 16,
+                    //           // backgroundImage:
+                    //           // AssetImage('assets/images/user.png'),
+                    //         )
+                    //       ],
+                    //     ),
+                    //     subTitle: Text(
+                    //       post['caption'] ?? 'No Caption',
+                    //       style: const TextStyle(fontSize: 14),
+                    //     ),
+                    //     description: Text(
+                    //       "Posted by ${post['user']['username']} on ${post['createdAt']}",
+                    //       style: const TextStyle(fontSize: 12),
+                    //     ),
+                    //   ),
+                    // );
+
+                    //     ListTile(
                     //   title: Text(post['title'] ?? 'No Title'),
                     //   subtitle: Text(post['caption'] ?? 'No Caption'),
                     //   leading: post['pictureUrl'] != null
