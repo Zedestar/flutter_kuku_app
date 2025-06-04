c18986cde174334b7b168588ab08c8db16625053
Melkizedeki <mmagota2@gmail.com>
2025-06-05 02:27:48 +0300

    The commit after refactoring post card and details post pae

--- lib/pages/general_post_details_view.dart
+++ lib/pages/general_post_details_view.dart
@@ -3,7 +3,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:kuku_app/graphql/graphql_queries.dart';
 import 'package:kuku_app/widgets/app_bar.dart';
 import 'package:kuku_app/widgets/post_card.dart';
-import 'package:kuku_app/constants/constant.dart';
+// import 'package:kuku_app/constants/constant.dart';
 
 class GeneralPostDetailsView extends StatefulWidget {
   const GeneralPostDetailsView({super.key, required this.post});
@@ -26,91 +26,158 @@ class _GeneralPostDetailsViewState extends State<GeneralPostDetailsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: theAppBar(context, "Post Details"),
       body: PostCard(
         post: widget.post,
         commentsWidget: GraphQLConsumer(
           builder: (GraphQLClient client) {
-            return FutureBuilder<QueryResult>(
-              future: client.query(
-                QueryOptions(
-                  document: gql(commentsOfPostQuery),
-                  variables: {
-                    'id': int.parse(widget.post['id']),
-                  },
-                ),
-              ),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasError) {
-                  return Center(
-                    child: Text("Error: ${snapshot.error}"),
-                  );
-                } else {
-                  final comments =
-                      snapshot.data?.data?['comments'] as List<dynamic> ?? [];
+            final options = WatchQueryOptions(
+              document: gql(commentsOfPostQuery),
+              variables: {
+                'id': int.parse(widget.post['id']),
+              },
+              pollInterval: const Duration(seconds: 1),
+              fetchPolicy: FetchPolicy.cacheAndNetwork,
+              fetchResults: true,
+              parserFn: (data) => data,
+            );
 
-                  if (comments.isEmpty) {
-                    return const Center(
-                      child: Text("No comments available"),
-                    );
-                  }
-                  return SizedBox(
-                    height: 300,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: comments.length,
-                      itemBuilder: (context, index) {
-                        final comment = comments[index];
-                        return ListTile(
-                          leading: CircleAvatar(
-                            radius: 16,
-                          ),
-                          title: Text(comment['body']),
-                          subtitle: Text(comment['user']['username']),
-                        );
-                      },
-                    ),
-                  );
+            final observableQuery = client.watchQuery(options);
+
+            return StreamBuilder<QueryResult>(
+              stream: observableQuery.stream,
+              initialData: observableQuery.latestResult,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text("Error: ${snapshot.error}"));
                 }
+
+                if (!snapshot.hasData || snapshot.data!.data == null) {
+                  return const Center(child: Text("No comments available"));
+                }
+
+                final comments =
+                    (snapshot.data!.data!['comments'] as List<dynamic>?) ?? [];
+
+                if (comments.isEmpty) {
+                  return const Center(child: Text("No comments available"));
+                }
+
+                return SizedBox(
+                  height: 300,
+                  child: ListView.builder(
+                    itemCount: comments.length,
+                    itemBuilder: (context, index) {
+                      final comment = comments[index];
+                      return ListTile(
+                        leading: CircleAvatar(radius: 16),
+                        title: Text(comment['body']),
+                        subtitle: Text(comment['user']['username']),
+                      );
+                    },
+                  ),
+                );
               },
             );
           },
         ),
-        formWidget: Form(
-          child: Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  controller: _commentInputController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Comment cannot be empty";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Add comment',
-                    hintText: 'Write comment here',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
+        // formWidget: Text("There will be a form here to add comments"),
+        formWidget: Padding(
+          padding: EdgeInsets.all(0),
+          child: Form(
+            key: _formKey,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _commentInputController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Comment cannot be empty";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Add comment',
+                      hintText: 'Write comment here',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.send,
-                  size: 30,
-                  color: Colors.lightBlueAccent,
+                IconButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("Submit: ${_commentInputController.text}");
+                      _commentInputController.clear();
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.send,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+            // return FutureBuilder<QueryResult>(
+            //   future: client.query(
+            //     WatchQueryOptions(
+            //         document: gql(commentsOfPostQuery),
+            //         variables: {
+            //           'id': int.parse(widget.post['id']),
+            //         },
+            //         pollInterval: const Duration(seconds: 1)),
+            //   ),
+            //   builder: (context, snapshot) {
+            //     if (snapshot.connectionState == ConnectionState.waiting) {
+            //       return const Center(
+            //         child: CircularProgressIndicator(),
+            //       );
+            //     } else if (snapshot.hasError) {
+            //       return Center(
+            //         child: Text("Error: ${snapshot.error}"),
+            //       );
+            //     } else {
+            //       final comments =
+            //           snapshot.data?.data?['comments'] as List<dynamic> ?? [];
+
+            //       if (comments.isEmpty) {
+            //         return const Center(
+            //           child: Text("No comments available"),
+            //         );
+            //       }
+            //       return SizedBox(
+            //         height: 300,
+            //         child: ListView.builder(
+            //           shrinkWrap: true,
+            //           itemCount: comments.length,
+            //           itemBuilder: (context, index) {
+            //             final comment = comments[index];
+            //             return ListTile(
+            //               leading: CircleAvatar(
+            //                 radius: 16,
+            //               ),
+            //               title: Text(comment['body']),
+            //               subtitle: Text(comment['user']['username']),
+            //             );
+            //           },
+            //         ),
+            //       );
+            //     }
+            //   },
+            // );
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -22,72 +22,74 @@ class PostCard extends StatelessWidget {
         children: [
           SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                    child: post['pictureUrl'] != null
+                        ? Image.network(
+                            post['pictureUrl'],
+                            width: double.infinity,
+                            height: 180,
+                            fit: BoxFit.cover,
+                          )
+                        : const SizedBox.shrink(),
                   ),
-                  child: post['pictureUrl'] != null
-                      ? Image.network(
-                          post['pictureUrl'],
-                          width: double.infinity,
-                          height: 180,
-                          fit: BoxFit.cover,
-                        )
-                      : const SizedBox.shrink(),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 4,
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 4,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          post['title'] ?? 'No Title',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Text(
+                          post['caption'] ?? 'No no Caption',
+                          style: const TextStyle(fontSize: 14),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              post['user']['username'] ?? 'Unknown',
+                              style: const TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.w600),
+                            ),
+                            Spacer(),
+                          ],
+                        ),
+                        Divider(
+                          color: Colors.grey,
+                          thickness: 1,
+                          indent: 2, // left padding
+                          endIndent: 16, // right padding
+                        ),
+                        SizedBox(height: 265, child: commentsWidget),
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        post['title'] ?? 'No Title',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      Text(
-                        post['caption'] ?? 'No no Caption',
-                        style: const TextStyle(fontSize: 14),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                        children: [
-                          CircleAvatar(),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            post['user']['username'] ?? 'Unknown',
-                            style: const TextStyle(
-                                fontSize: 12, fontWeight: FontWeight.w600),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                      Divider(
-                        color: Colors.grey,
-                        thickness: 1,
-                        indent: 2, // left padding
-                        endIndent: 16, // right padding
-                      ),
-                      SizedBox(child: commentsWidget),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-          Spacer(),
           Expanded(child: formWidget),
         ],
       ),
